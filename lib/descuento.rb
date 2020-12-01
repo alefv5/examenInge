6d460ed744bf3e016ba308dff1cd0aa3bccf8bc5
alefv5 <alejandrafv5@gmail.com>
2020-12-01 09:55:28 -0400

    2da y 3ra prueba con descuento

--- lib/descuento.rb
+++ lib/descuento.rb
@@ -1,6 +1,14 @@
 class Descuento
     def calcularDescuentoPorItems(cantItems,precio_unitario)
-        descuentoItems=(cantItems*precio_unitario)*0.03
+        if(cantItems >= 0 && cantItems < 3000)
+            descuentoItems=(cantItems*precio_unitario)*0.03
+        end
+        if(cantItems >= 3000 && cantItems < 5000)
+            descuentoItems=(cantItems*precio_unitario)*0.05
+        end
+        if(cantItems >= 7000 && cantItems < 10000)
+            descuentoItems=(cantItems*precio_unitario)*0.05
+        end
         return descuentoItems
     end
 end
--- spec/descuento_spec.rb
+++ spec/descuento_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Descuento do
         @descuento = Descuento.new
         expect(@descuento.calcularDescuentoPorItems(1000,5)).to eq(150)
     end
+    it 'Deberia 750 de descuento' do
+        @descuento = Descuento.new
+        expect(@descuento.calcularDescuentoPorItems(3000,5)).to eq(750)
+    end
+    it 'Deberia 1750 de descuento' do
+        @descuento = Descuento.new
+        expect(@descuento.calcularDescuentoPorItems(7000,5)).to eq(1750)
+    end
+    it 'Deberia descuento total' do
+        @descuento = Descuento.new
+        expect(@descuento.calcularDescuentoTotal(1000,5,'UT')).to eq()
+    end
 end
